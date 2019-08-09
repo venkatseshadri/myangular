3cdf7d6e14a3c647688816d39912055d1d5fb8dc
vseshadri <vseshadri@manh.com>
2019-08-10 00:12:12 +0530

    moved server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
+
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+var port = server.address().port;
+console.log("App now running on port", port);
+});
+
+// CONTACTS API ROUTES BELOW
